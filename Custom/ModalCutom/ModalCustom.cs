f290c31ddf1d974d6273b2df17286a57ff22f4d7
Константин Несутулов <186141@edu.fa.ru>
2021-10-05 19:03:23 +0300

    #K:Перенос внешних проектов в решение

---
+++ Custom/ModalCutom/ModalCustom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ModalCutom
+{
+    
+    public class ModalCustom : ContentControl
+    {
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(ModalCustom),
+                new PropertyMetadata(false));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        static ModalCustom()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ModalCustom), new FrameworkPropertyMetadata(typeof(ModalCustom)));
+            BackgroundProperty.OverrideMetadata(typeof(ModalCustom), new FrameworkPropertyMetadata(CreateDefaultBackground()));
+        }
+
+        private static object CreateDefaultBackground()
+        {
+            return new SolidColorBrush(Colors.Black)
+            {
+                Opacity = 0.3
+            };
+        }
+    }
+}
